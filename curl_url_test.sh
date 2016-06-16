35f9d8db5f07f7cdf545bf7b1556c3c912b4f51c
James White <james@jmwhite.co.uk>
2016-06-16 11:14:57 +0100

    Version 0.2

--- curl_url_test.sh
+++ curl_url_test.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # curl_url_test.sh
 # Author: James White (james.white@centralnottingham.ac.uk)
+# Source: https://github.com/centralcollegenottingham/url-tester
+# Version: 0.2
 # Summary: Uses curl to test one or more URLs and the corresponding HTTP status code
 
 # Description:
@@ -8,15 +10,16 @@
 # Useful for checking batches of URLs quickly.
 #
 
+TIMESTAMP=$(date +%s)
 URL_LIST_FILE="url_list.txt"
-TEST_RESULT_FILE="curl_url_test_result.txt"
+TEST_RESULT_FILE="curl_url_test_result-${TIMESTAMP}.txt"
 
 if [ ! -f "${URL_LIST_FILE}" ] ; then
     echo "URL list doesn't exist."
     exit 1
 fi
 
-echo "Checking URL's list. Results will be recorded to ${TEST_RESULT_FILE}"
+echo "Checking URLs list. Results will be recorded to ${TEST_RESULT_FILE}"
 echo "Test started at: $(date)" >> ${TEST_RESULT_FILE}
 # loop through file with URL list and record HTTP status code against tested URL
 while read url ; do
